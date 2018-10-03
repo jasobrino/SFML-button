a1f3e1b238147d7019ac3b94d571c3bbf5455802
Juan <jasobrino66@gmail.com>
2018-10-03 18:20:21 +0200

    SFML button class

---
+++ boton.cpp
@@ -0,0 +1,116 @@
+#include "boton.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+// Constructores
+Boton::Boton(sf::Vector2f tam,  const std::string &txtbot): sf::RectangleShape(tam) {
+    this->setText(txtbot);
+    init();
+}
+// Pasamos un vector con el tamaño, el id del boton y la rutina de callback
+Boton::Boton(sf::Vector2f tam, uint16_t id, void(*callback)(uint16_t), const std::string &txtbot): Boton(tam, txtbot) {
+    this->id = id;
+    this->cbkFunc = callback;
+}
+
+// Método de inicialización
+void Boton::init() {
+  this->setFillColor(this->colFill); //color de fondo botón
+  //cargamos la fuente
+  if( !this->font.loadFromFile("Boton/fonts/DejaVuSerif.ttf")) {
+    cout << "no pudo cargarse la fuente." << endl;
+  }
+  this->textBtn.setFont(this->font);
+  this->textBtn.setFillColor(this->textColor);
+//  this->tx_pressed.loadFromFile("Boton/boton.png");
+//  this->setTexturePressed(tx_pressed);
+//  this->setTextureUnpressed(tx_pressed);
+}
+
+// Métodos de clase
+
+// Carga una textura para el boton
+void Boton::setTextureButton(const string &file, bool pressed){
+
+    if(pressed) {
+       this->tx_pressed.loadFromFile(file);
+    } else {
+        if(this->tx_unpressed.loadFromFile(file)){
+            this->textureButton = &this->tx_unpressed;
+       }
+
+    }
+}
+
+/* checkMouse:
+    event: Manejador de eventos de la llamada
+    w: Ventana
+    container: en el caso de que el boton deba dibujarse dentro de un objeto shape contenedor
+*/
+void Boton::checkMouse(sf::Event &event, sf::RenderWindow &w) {
+
+    sf::Vector2i posMouse = sf::Mouse::getPosition(w);
+    // Si se define shape contenedor y existe RenderTexture, desplazamos las coordenadas del mouse
+    // de lo contrario se tendrá en cuenta la ventana como superficie de render
+    if( this->rndrShape && this->rndrText ) {
+        posMouse -= sf::Vector2i(this->rndrShape->getGlobalBounds().left ,this->rndrShape->getGlobalBounds().top);
+    }
+     // comprobamos que el ratón se pulsó dentro del botón
+    if( this->getGlobalBounds().contains(posMouse.x, posMouse.y) ) {
+        sf::Mouse::Button bt =  event.mouseButton.button;
+        if( bt == sf::Mouse::Left ) {
+            if ( event.type == sf::Event::MouseButtonPressed )  {
+                if(this->textureButton)
+                    this->textureButton = &tx_pressed;
+                else
+                    this->setFillColor(this->colClick);
+
+                if( this->cbkFunc ) // si se asignó callback llamamos a la función
+                    this->cbkFunc(this->id);
+                else
+                    cout << "funcion callback no definida." << endl;
+            }
+        }
+    }
+    // cuando soltamos el botón en cualquier parte
+    if ( event.type == sf::Event::MouseButtonReleased )  {
+        if(this->textureButton)
+            this->textureButton = &tx_unpressed;
+        else
+            this->setFillColor(this->colFill);
+    }
+}
+
+// Asignamos texto al botón en base a la altura del mismo
+void Boton::setText(const std::string &text) {
+     this->textBtn.setString(text);
+     this->textBtn.setCharacterSize(this->getSize().y * 0.7f);
+}
+
+// Renderiza el boton en la ventana o en el contenedor si existe
+void Boton::show(sf::RenderWindow &w) {
+    setPositionTxt();
+    if( this->textureButton ) {
+        this->setTexture(textureButton, true);
+    }
+    if(this->rndrText) { // existe un renderTexture
+        this->rndrText->draw(*this);
+        this->rndrText->draw(this->textBtn);
+        rndrText->display();
+    } else { // renderizamos directamente en la ventana
+        w.draw(*this);
+        w.draw(this->textBtn);
+    }
+}
+
+// Calcula la posición centrada del texto dentro del botón
+void Boton::setPositionTxt() {
+    uint16_t wtxt = this->textBtn.getGlobalBounds().width;
+    uint16_t wbtn = this->getGlobalBounds().width;
+    uint16_t htxt = this->textBtn.getGlobalBounds().height;
+    uint16_t hbtn = this->getGlobalBounds().height;
+    // Centramos el texto en el botón
+    this->textBtn.setPosition( this->getPosition().x + (wbtn-wtxt)/2, this->getPosition().y + (hbtn-htxt)/2 - 5);
+}
+
---
+++ boton.h
@@ -0,0 +1,50 @@
+#ifndef BOTON_HPP_INCLUDED
+#define BOTON_HPP_INCLUDED
+#include <SFML/Graphics.hpp>
+
+class Boton: public sf::RectangleShape {
+
+    uint16_t id {};
+    sf::Texture tx_pressed {};
+    sf::Texture tx_unpressed {};
+    sf::Texture *textureButton {nullptr};
+    // definición de colores para el botón
+    sf::Color colFill {sf::Color::Cyan};
+    sf::Color colClick {sf::Color(0xaa,0xaa,0xbb,0xff)};
+    sf::Color textColor {sf::Color::Black};
+    // atributos para el texto
+    sf::Font font;
+    sf::Text textBtn;
+    sf::RenderTexture *rndrText {nullptr};
+    sf::Shape *rndrShape {nullptr};
+
+    void (*cbkFunc)(uint16_t) {nullptr}; //guarda el puntero de la rutina de callback
+    // función de inicialización
+    void init();
+
+public:
+    // Constructor por defecto: con tamaño inicial fijo
+    Boton(): Boton(sf::Vector2f(70,30)) {}
+    // Constructor paso de vector con tamaño y texto(opcional)
+    Boton(sf::Vector2f tam,  const std::string &txtbot="?");
+    // Constructor paso tamaño, id y rutina de callback
+    Boton(sf::Vector2f tam, uint16_t id, void(*callback)(uint16_t), const std::string &txtbot="?");
+    // getters y setters
+    uint16_t getId() { return this->id; }
+    void setId(uint16_t id) { this->id = id; }
+    void setRenderTarget(sf::RenderTexture *rndtx, sf::Shape *rndshp) { this->rndrText = rndtx; this->rndrShape = rndshp; }
+    void setText(const std::string &text);
+    void setBtnColor(const sf::Color col) { this->colFill = col; this->setFillColor(col); }
+    void setTextColor(const sf::Color col) { this->textColor = col; this->textBtn.setFillColor(this->textColor); }
+    void setTextureButton(const std::string &file, bool pressed); //carga la textura del boton pulsado(true)/sin pulsar(false)
+    // checkMouse: recibe el manejador de eventos y la ventana para comprobar si se pulsa dentro del mismo
+    void checkMouse(sf::Event &event, sf::RenderWindow &w);
+    // manejador de la rutina de callback
+    void handle(void(*callback)(uint16_t)) { this->cbkFunc = callback; }
+    void show(sf::RenderWindow &w);
+    //void setPosition(const sf::Vector2f& pos);
+    void setPositionTxt();
+};
+
+
+#endif // BOTON_HPP_INCLUDED
